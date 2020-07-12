4ff6f96d1888c0882c632f38f6df28265d2219af
Eric Ahnell <eric.ahnell@puttysoftware.com>
2020-07-12 14:06:54 -0400

    Remove old headers.

--- AvatarConstructionKit-Java/src/com/puttysoftware/ack/AvatarConstructionKit.java
+++ AvatarConstructionKit-Java/src/com/puttysoftware/ack/AvatarConstructionKit.java
@@ -1,9 +1,3 @@
-/*  FantastleReboot: An RPG
-Copyright (C) 2011-2012 Eric Ahnell
-
-
-Any questions should be directed to the author via email at: products@puttysoftware.com
- */
 package com.puttysoftware.ack;
 
 import java.awt.Color;
--- AvatarConstructionKit-Java/src/com/puttysoftware/ack/internal/HexBytes.java
+++ AvatarConstructionKit-Java/src/com/puttysoftware/ack/internal/HexBytes.java
@@ -1,8 +1,3 @@
-/*  Diane Game Engine
-Copyleft (C) 2019 Eric Ahnell
-
-Any questions should be directed to the author via email at: support@puttysoftware.com
- */
 package com.puttysoftware.ack.internal;
 
 public class HexBytes {
